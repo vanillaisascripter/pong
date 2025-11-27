3a4efd56859ca543b394a9ce362cd7cb1837dd3e
vanillaisascripter <vmain99@outlook.com>
2025-11-26 19:35:38 -0500

    Create controls.js

---
+++ controls.js
@@ -0,0 +1,11 @@
+export class Controls {
+  constructor(canvas, physics) {
+    this.canvas = canvas;
+    this.physics = physics;
+  }
+
+  init() {
+    // Add touch/mouse/keyboard event listeners
+    // Update paddle positions in physics.state
+  }
+}
